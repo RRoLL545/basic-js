e7f48a8f969d6f381ef8ab28bc0d1735f31757a8
RRoLL545 <motom@yandex.ru>
2020-10-01 04:51:38 +0400

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if (Array.isArray(members) !== true ) {
+    return false
+  }
+  let teamName = '';
+	for ( let i = 0; i < members.length; i++) {
+  	if (typeof members[i] === 'string') {
+      let trimmed = members[i].trim();
+    	teamName = teamName + trimmed[0];
+    }
+  }
+
+  if (teamName === '') {
+    return false;
+}
+
+  teamName=teamName.toUpperCase();
+  teamName=teamName.split('').sort().join('');
+
+  
+  
+  return teamName;
 };
